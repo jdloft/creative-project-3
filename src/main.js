b33612ba02774c257506dd816de661e9f6e5e6b2
Jamison Lofthouse <jamison.lofthouse@gmail.com>
2021-11-06 09:49:17 -0600

    Add todo from lab

--- src/main.js
+++ src/main.js
@@ -1,10 +1,66 @@
 import Vue from 'vue'
+import Vuex from 'vuex'
+
 import App from './App.vue'
 import router from './router'
 
 Vue.config.productionTip = false
+Vue.use(Vuex)
+
+const store = new Vuex.Store({
+  state: {
+    todos: [
+      {
+        text: "make an app",
+        completed: false,
+      },
+      {
+        text: "declare victory",
+        completed: false,
+      },
+      {
+        text: "profit",
+        completed: false,
+      },
+    ],
+    message: "",
+    show: "all",
+    drag: {},
+  },
+  mutations: {
+    addItem(state) {
+      state.todos.push({ text: state.message, completed: false });
+      state.message = "";
+    },
+    deleteItem(state, item) {
+      var index = state.todos.indexOf(item);
+      if (index > -1) state.todos.splice(index, 1);
+    },
+    setShow(state, show) {
+      state.show = show;
+    },
+    setMessage(state, message) {
+      state.message = message;
+    },
+    deleteCompleted(state) {
+      state.todos = state.todos.filter((item) => {
+        return !item.completed;
+      });
+    },
+    dragItem(state, item) {
+      state.drag = item;
+    },
+    dropItem(state, item) {
+      const indexItem = state.todos.indexOf(this.$root.drag);
+      const indexTarget = state.todos.indexOf(item);
+      state.todos.splice(indexItem, 1);
+      state.todos.splice(indexTarget, 0, state.drag);
+    },
+  }
+})
 
 new Vue({
   router,
-  render: h => h(App)
+  render: h => h(App),
+  store: store,
 }).$mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,157 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>A List of Things To Do</h1>
+    <p v-show="activeTodos.length === 0">
+      You are done with all your tasks! Good job!
+    </p>
+    <form v-on:submit.prevent="addItem">
+      <input type="text" :value="message" @input="updateMessage" />
+      <button type="submit">Add</button>
+    </form>
+    <div class="controls">
+      <button v-on:click="showAll()">Show All</button>
+      <button v-on:click="showActive()">Show Active</button>
+      <button v-on:click="showCompleted()">Show Completed</button>
+      <button v-on:click="deleteCompleted()">Delete Completed</button>
+    </div>
+    <ul>
+      <li
+        v-for="item in filteredTodos"
+        :key="item.text"
+        draggable="true"
+        v-on:dragstart="dragItem(item)"
+        v-on:dragover.prevent
+        v-on:drop="dropItem(item)"
+      >
+        <input type="checkbox" v-model="item.completed" />
+        <label v-bind:class="{ completed: item.completed }">{{
+          item.text
+        }}</label>
+        <button v-on:click="deleteItem(item)" class="delete">Delete</button>
+      </li>
+    </ul>
   </div>
 </template>
 
+<style scoped>
+body {
+  font-family: "Arvo";
+  font-size: 16px;
+  padding: 20px 100px 0px 100px;
+  background: #f3f3f3;
+}
+
+/* List */
+
+ul {
+  list-style: none;
+}
+
+li {
+  background: #fff;
+  width: 500px;
+  min-height: 30px;
+  padding: 10px;
+  margin-bottom: 10px;
+  font-size: 1em;
+  display: flex;
+  align-items: center;
+}
+
+.delete {
+  display: none;
+  margin-left: auto;
+}
+
+li:hover .delete {
+  display: block;
+}
+
+label {
+  width: 400px;
+}
+
+.completed {
+  text-decoration: line-through;
+}
+
+/* Form */
+
+input[type="checkbox"] {
+  transform: scale(1.5);
+  margin-right: 10px;
+}
+
+input[type="text"] {
+  font-size: 1em;
+}
+
+button {
+  font-family: "Arvo";
+  font-size: 1em;
+}
+
+/* Controls */
+
+.controls {
+  margin-top: 20px;
+}
+</style>
+
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
+  name: "Home",
+  computed: {
+    activeTodos() {
+      return this.$store.state.todos.filter((item) => {
+        return !item.completed;
+      });
+    },
+    filteredTodos() {
+      if (this.$store.state.show === "active")
+        return this.$store.state.todos.filter((item) => {
+          return !item.completed;
+        });
+      if (this.$store.state.show === "completed")
+        return this.$store.state.todos.filter((item) => {
+          return item.completed;
+        });
+      return this.$store.state.todos;
+    },
+    message() {
+      return this.$store.message;
+    }
+  },
+  methods: {
+    addItem() {
+      this.$store.commit("addItem");
+    },
+    deleteItem(item) {
+      this.$store.commit("deleteItem", item);
+    },
+    deleteCompleted() {
+      this.$store.commit("deleteCompleted");
+    },
+    dragItem(item) {
+      this.$store.commit("dragItem", item);
+    },
+    dropItem(item) {
+      this.$store.commit("dropItem", item);
+    },
+    showAll() {
+      this.$store.commit("setShow", "all");
+    },
+    showActive() {
+      this.$store.commit("setShow", "active");
+    },
+    showCompleted() {
+      this.$store.commit("setShow", "completed");
+    },
+    updateMessage(e) {
+      this.$store.commit("setMessage", e.target.value);
+    }
   }
-}
+};
 </script>
